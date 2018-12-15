6d774d226c83e8afd3aa4be1b959fc8080247fc6
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2018-12-15 14:36:23 +0300

    Мат моделирование - описывающее себя число

---
+++ src/мат_модель/Main.java
@@ -0,0 +1,223 @@
+package мат_модель;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+import java.util.function.BiFunction;
+
+/**
+ * Задача 588
+ * Найти описывающее себя число
+ * b до 1000
+ */
+public class Main {
+
+    static private MyScanner in;
+    static private PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        in = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+
+        int b = in.nextInt();
+        if (b < 4 || b == 6) {
+            out.println(-1);
+        } else if (b == 4) {
+            out.println("2\n1\n0\n1");
+        } else if (b == 5) {
+            out.println("1\n2\n0\n0\n2");
+        } else {
+            out.println("2\n1");
+            for (int i = 0; i < b - 7; i++) {
+                out.println("0");
+            }
+            out.println("1\n0\n0\n0");
+            out.println(b - 4);
+        }
+    }
+
+    static void tryToFind(int b) {
+        loop:
+        for (long n = 0; ; n++) {
+            String s = Long.toString(n, b);
+            if (s.length() > b) {
+                break;
+            }
+            if (s.length() < b) {
+                continue;
+            }
+
+            int[] cnt = new int[b];
+            for (char ch : s.toCharArray()) {
+                int c = ch - '0';
+                if (c == 0) {
+                    cnt[b - 1]++;
+                    if (cnt[b - 1] > 9) continue loop;
+                } else {
+                    cnt[c - 1]++;
+                    if (cnt[c - 1] > 9) continue loop;
+                }
+            }
+
+            StringBuilder res = new StringBuilder();
+            for (int a : cnt) {
+                res.append(a);
+            }
+
+            if (res.toString().equals(s)) {
+                System.out.println(res);
+            }
+        }
+    }
+}
+
+class MyScanner implements Closeable {
+
+    public static final String CP1251 = "cp1251";
+
+    private final BufferedReader br;
+    private StringTokenizer st;
+    private String last;
+
+    public MyScanner() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public MyScanner(String path) throws IOException {
+        br = new BufferedReader(new FileReader(path));
+    }
+
+    public MyScanner(String path, String decoder) throws IOException {
+        br = new BufferedReader(new InputStreamReader(new FileInputStream(path), decoder));
+    }
+
+    public void close() throws IOException {
+        br.close();
+    }
+
+    public String next() throws IOException {
+        while (st == null || !st.hasMoreElements())
+            st = new StringTokenizer(br.readLine());
+        last = null;
+        return st.nextToken();
+    }
+
+    public String next(String delim) throws IOException {
+        while (st == null || !st.hasMoreElements())
+            st = new StringTokenizer(br.readLine());
+        last = null;
+        return st.nextToken(delim);
+    }
+
+    public String nextLine() throws IOException {
+        st = null;
+        return (last == null) ? br.readLine() : last;
+    }
+
+    public boolean hasNext() {
+        if (st != null && st.hasMoreElements())
+            return true;
+
+        try {
+            while (st == null || !st.hasMoreElements()) {
+                last = br.readLine();
+                st = new StringTokenizer(last);
+            }
+        }
+        catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public String[] nextStrings(int n) throws IOException {
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = next();
+        return arr;
+    }
+
+    public String[] nextLines(int n) throws IOException {
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextLine();
+        return arr;
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    public int[] nextInts(int n) throws IOException {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextInt();
+        return arr;
+    }
+
+    public Integer[] nextIntegers(int n) throws IOException {
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextInt();
+        return arr;
+    }
+
+    public int[][] next2Ints(int n, int m) throws IOException {
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = nextInt();
+        return arr;
+    }
+
+    public long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    public long[] nextLongs(int n) throws IOException {
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextLong();
+        return arr;
+    }
+
+    public long[][] next2Longs(int n, int m) throws IOException {
+        long[][] arr = new long[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = nextLong();
+        return arr;
+    }
+
+    public double nextDouble() throws IOException {
+        return Double.parseDouble(next().replace(',', '.'));
+    }
+
+    public double[] nextDoubles(int size) throws IOException {
+        double[] arr = new double[size];
+        for (int i = 0; i < size; i++)
+            arr[i] = nextDouble();
+        return arr;
+    }
+
+    public double[][] next2Doubles(int n, int m) throws IOException {
+        double[][] arr = new double[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = nextDouble();
+        return arr;
+    }
+
+    public boolean nextBool() throws IOException {
+        String s = next();
+        if (s.equalsIgnoreCase("true") || s.equals("1"))
+            return true;
+
+        if (s.equalsIgnoreCase("false") || s.equals("0"))
+            return false;
+
+        throw new IOException("Boolean expected, String found!");
+    }
+}
