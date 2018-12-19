8dc70b6c6d7abeba9a81baaf228f3e74585d0c66
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2018-12-20 00:54:15 +0300

    Графы - мини-пятнажки

---
+++ src/графы/t509/Main.java
@@ -0,0 +1,238 @@
+package графы.t509;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Задача 509
+ * Вывести количество переходов фишки
+ */
+public class Main {
+
+    static private MyScanner in;
+    static private PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        in = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+
+        String start = in.next() + in.next();
+        String end = in.next() + in.next();
+
+        out.println(new Solver(start, end).solve());
+    }
+
+}
+
+class Solver {
+    private String start, end;
+    private int[][] moves = new int[][] {
+            {1, 4},
+            {0, 2, 5},
+            {1, 3, 6},
+            {2, 7},
+            {0, 5},
+            {4, 1, 6},
+            {5, 2, 7},
+            {6, 3}
+    };
+
+    public Solver(String start, String end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int solve() {
+        if (start.equals(end)) {
+            return 0;
+        }
+
+        HashMap<String, Integer> g = new HashMap<>();
+        LinkedList<String> queue = new LinkedList<>();
+        queue.add(start);
+        g.put(start, 0);
+
+        while(!queue.isEmpty()) {
+            String cur = queue.poll();
+            int times = g.get(cur);
+
+            int hashPos = cur.indexOf('#');
+            for(String var: generate(cur, hashPos)) {
+                if (var.equals(end)) {
+                    return times + 1;
+                }
+
+                if (!g.containsKey(var)) {
+                    g.put(var, times + 1);
+                    queue.add(var);
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private LinkedList<String> generate(String s, int pos) {
+        LinkedList<String> vars = new LinkedList<>();
+
+        for (int move: moves[pos]) {
+            StringBuilder sb = new StringBuilder(s);
+            sb.setCharAt(pos, sb.charAt(move));
+            sb.setCharAt(move, '#');
+
+            vars.add(sb.toString());
+        }
+
+        return vars;
+    }
+}
+
+class MyScanner implements Closeable {
+
+    public static final String CP1251 = "cp1251";
+
+    private final BufferedReader br;
+    private StringTokenizer st;
+    private String last;
+
+    public MyScanner() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public MyScanner(String path) throws IOException {
+        br = new BufferedReader(new FileReader(path));
+    }
+
+    public MyScanner(String path, String decoder) throws IOException {
+        br = new BufferedReader(new InputStreamReader(new FileInputStream(path), decoder));
+    }
+
+    public void close() throws IOException {
+        br.close();
+    }
+
+    public String next() throws IOException {
+        while (st == null || !st.hasMoreElements())
+            st = new StringTokenizer(br.readLine());
+        last = null;
+        return st.nextToken();
+    }
+
+    public String next(String delim) throws IOException {
+        while (st == null || !st.hasMoreElements())
+            st = new StringTokenizer(br.readLine());
+        last = null;
+        return st.nextToken(delim);
+    }
+
+    public String nextLine() throws IOException {
+        st = null;
+        return (last == null) ? br.readLine() : last;
+    }
+
+    public boolean hasNext() {
+        if (st != null && st.hasMoreElements())
+            return true;
+
+        try {
+            while (st == null || !st.hasMoreElements()) {
+                last = br.readLine();
+                st = new StringTokenizer(last);
+            }
+        }
+        catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public String[] nextStrings(int n) throws IOException {
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = next();
+        return arr;
+    }
+
+    public String[] nextLines(int n) throws IOException {
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextLine();
+        return arr;
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    public int[] nextInts(int n) throws IOException {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextInt();
+        return arr;
+    }
+
+    public Integer[] nextIntegers(int n) throws IOException {
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextInt();
+        return arr;
+    }
+
+    public int[][] next2Ints(int n, int m) throws IOException {
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = nextInt();
+        return arr;
+    }
+
+    public long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    public long[] nextLongs(int n) throws IOException {
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextLong();
+        return arr;
+    }
+
+    public long[][] next2Longs(int n, int m) throws IOException {
+        long[][] arr = new long[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = nextLong();
+        return arr;
+    }
+
+    public double nextDouble() throws IOException {
+        return Double.parseDouble(next().replace(',', '.'));
+    }
+
+    public double[] nextDoubles(int size) throws IOException {
+        double[] arr = new double[size];
+        for (int i = 0; i < size; i++)
+            arr[i] = nextDouble();
+        return arr;
+    }
+
+    public double[][] next2Doubles(int n, int m) throws IOException {
+        double[][] arr = new double[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = nextDouble();
+        return arr;
+    }
+
+    public boolean nextBool() throws IOException {
+        String s = next();
+        if (s.equalsIgnoreCase("true") || s.equals("1"))
+            return true;
+
+        if (s.equalsIgnoreCase("false") || s.equals("0"))
+            return false;
+
+        throw new IOException("Boolean expected, String found!");
+    }
+}
