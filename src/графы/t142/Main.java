f03b6ced424157819b0c96cbf2a1a9281fc59b2a
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2018-12-20 01:16:01 +0300

    Графы - минимальное остовное дерево

---
+++ src/графы/t142/Main.java
@@ -0,0 +1,223 @@
+package графы.t142;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Задача 142
+ * Вывести вес минимального остовного дерева
+ * N до 100, M до 6000
+ */
+public class Main {
+
+    static private MyScanner in;
+    static private PrintWriter out;
+
+    public static void main(String[] args) throws Exception {
+        in = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        int[][] g = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(g[i], Integer.MAX_VALUE);
+        }
+
+        for (int i = 0; i < m; i++) {
+            int a = in.nextInt() - 1;
+            int b = in.nextInt() - 1;
+            int c = in.nextInt();
+
+            g[a][b] = c;
+            g[b][a] = c;
+        }
+
+        boolean[] used = new boolean[n];
+        int[] minE = new int[n];
+        Arrays.fill(minE, Integer.MAX_VALUE);
+        int[] selE = new int[n];
+        Arrays.fill(selE, -1);
+
+        minE[0] = 0;
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int v = -1;
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && (v == -1 || minE[j] < minE[v])) {
+                    v = j;
+                }
+            }
+
+            if (minE[v] == Integer.MAX_VALUE) {
+                throw new Exception("NO ANSWER");
+            }
+
+            used[v] = true;
+            if (selE[v] != -1) {
+                sum += g[v][selE[v]];
+            }
+
+            for (int to = 0; to < n; to++) {
+                if (g[v][to] < minE[to]) {
+                    minE[to] = g[v][to];
+                    selE[to] = v;
+                }
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
+
+class MyScanner implements Closeable {
+
+    public static final String CP1251 = "cp1251";
+
+    private final BufferedReader br;
+    private StringTokenizer st;
+    private String last;
+
+    public MyScanner() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public MyScanner(String path) throws IOException {
+        br = new BufferedReader(new FileReader(path));
+    }
+
+    public MyScanner(String path, String decoder) throws IOException {
+        br = new BufferedReader(new InputStreamReader(new FileInputStream(path), decoder));
+    }
+
+    public void close() throws IOException {
+        br.close();
+    }
+
+    public String next() throws IOException {
+        while (st == null || !st.hasMoreElements())
+            st = new StringTokenizer(br.readLine());
+        last = null;
+        return st.nextToken();
+    }
+
+    public String next(String delim) throws IOException {
+        while (st == null || !st.hasMoreElements())
+            st = new StringTokenizer(br.readLine());
+        last = null;
+        return st.nextToken(delim);
+    }
+
+    public String nextLine() throws IOException {
+        st = null;
+        return (last == null) ? br.readLine() : last;
+    }
+
+    public boolean hasNext() {
+        if (st != null && st.hasMoreElements())
+            return true;
+
+        try {
+            while (st == null || !st.hasMoreElements()) {
+                last = br.readLine();
+                st = new StringTokenizer(last);
+            }
+        }
+        catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public String[] nextStrings(int n) throws IOException {
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = next();
+        return arr;
+    }
+
+    public String[] nextLines(int n) throws IOException {
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextLine();
+        return arr;
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    public int[] nextInts(int n) throws IOException {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextInt();
+        return arr;
+    }
+
+    public Integer[] nextIntegers(int n) throws IOException {
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextInt();
+        return arr;
+    }
+
+    public int[][] next2Ints(int n, int m) throws IOException {
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = nextInt();
+        return arr;
+    }
+
+    public long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    public long[] nextLongs(int n) throws IOException {
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = nextLong();
+        return arr;
+    }
+
+    public long[][] next2Longs(int n, int m) throws IOException {
+        long[][] arr = new long[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = nextLong();
+        return arr;
+    }
+
+    public double nextDouble() throws IOException {
+        return Double.parseDouble(next().replace(',', '.'));
+    }
+
+    public double[] nextDoubles(int size) throws IOException {
+        double[] arr = new double[size];
+        for (int i = 0; i < size; i++)
+            arr[i] = nextDouble();
+        return arr;
+    }
+
+    public double[][] next2Doubles(int n, int m) throws IOException {
+        double[][] arr = new double[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = nextDouble();
+        return arr;
+    }
+
+    public boolean nextBool() throws IOException {
+        String s = next();
+        if (s.equalsIgnoreCase("true") || s.equals("1"))
+            return true;
+
+        if (s.equalsIgnoreCase("false") || s.equals("0"))
+            return false;
+
+        throw new IOException("Boolean expected, String found!");
+    }
+}
